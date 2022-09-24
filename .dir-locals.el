461f61ee3c8684c98ee5ccfa895bf76f52b0d6c4
ajchemist <1694505+aJchemist@users.noreply.github.com>
2022-09-24 21:06:13 +0900

    Updated github workflows

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,3 @@
-((nil
-  (cider-figwheel-main-default-options . "dev")
-  (cider-default-cljs-repl . shadow)
-  )
- (clojure-mode
-  (cider-clojure-cli-aliases . "test")
+((clojure-mode
+  (cider-clojure-cli-aliases . ":test")
   (clojure-local-source-test-path . "src/test")))
